e42cfe55bf7c9a515a29cc6007d5d91843b09e13
satyasibbena <79448111+satyasibbena@users.noreply.github.com>
2021-09-30 11:38:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,17 +9,9 @@ def turnoffthelight(update, context):
 def turnonthelight(update, context):
   context.bot.send_message(chat_id=update.effective_chat.id, text="Bulb turned on")
   send_value(1)
-  
-def turnoffthefan(update, context):
-  context.bot.send_message(chat_id=update.effective_chat.id, text="Fan turned off")
-  send_value(0)
-
-def turnonthefan(update, context):
-  context.bot.send_message(chat_id=update.effective_chat.id, text="Fan turned on")
-  send_value(1)
 
 def send_value(value):
-  feed = aio.feeds('Light')
+  feed = aio.feeds('Bedroom Light')
   aio.send_data(feed.key,value)
 def input_message(update, context):
   text=update.message.text
@@ -30,24 +22,12 @@ def input_message(update, context):
     send_value(0)
     context.bot.send_message(chat_id=update.effective_chat.id, text="Bulb turned off")
   
-def send_value(value):
-  feed = aio.feeds('Fan')
-  aio.send_data(feed.key,value)
-def input_message(update, context):
-  text=update.message.text
-  if text =="turnonthefan":
-    send_value(1)
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Fan turned on")
-  elif text =="turnoffthefan":
-    send_value(0)
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Fan turned off")
+
   
 def start(update, context):
   start_message='''
 /turnoffthelight or 'turn off':To turn off the bulb ,sends value=0 in feed
 /turnonthelight or 'turn on':To turn on the bulb ,sends value=1 in feed
-/turnoffthefan or 'turn off':To turn off the fan ,sends value=0 in feed
-/turnonthefan or 'turn on':To turn on the fan ,sends value=1 in feed
 '''
   context.bot.send_message(chat_id=update.effective_chat.id, text=start_message)
 ADAFRUIT_IO_USERNAME = os.getenv('ADAFRUIT_IO_USERNAME')
@@ -58,8 +38,6 @@ updater = Updater(TOKEN, use_context=True)
 dispatcher = updater.dispatcher
 dispatcher.add_handler(CommandHandler('turnoffthelight',turnoffthelight))
 dispatcher.add_handler(CommandHandler('turnonthelight',turnonthelight))
-dispatcher.add_handler(CommandHandler('turnoffthefan',turnoffthefan))
-dispatcher.add_handler(CommandHandler('turnonthefan',turnonthefan))
 dispatcher.add_handler(CommandHandler('start',start))
 dispatcher.add_handler(MessageHandler(Filters.text & (~Filters.command),input_message))
 updater.start_polling()
